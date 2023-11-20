c064d37db713e22cb8fd89b7df95f94cb6c589f0
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2023-11-20 16:56:06 +0500

    Установка связи с MySQL сервером

--- WinDbTestApp/MySqlDbHelper.cs
+++ WinDbTestApp/MySqlDbHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,28 @@ using System.Threading.Tasks;
 
 namespace WinDbTestApp
 {
-    internal class MySqlDbHelper
+    public class MySqlDbHelper
     {
+        public static string DoDbTest()
+        {
+            string s = " ";
+
+            string strCnn = "Server=localhost;User ID=root;Password=root;Database=arnion";
+            using (var cnn = new MySqlConnection(strCnn))
+            {
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "SELECT `categories_name` FROM `goods_categories` WHERE `id_categories` = 9";
+                cnn.Open();
+
+                var rdr = cmd.ExecuteReader();
+
+                while (rdr.Read())
+                {
+                    // get the results of each column
+                    s += (string)rdr[0].ToString() + " ";
+                }
+            }
+                return s;
+        }
     }
 }
