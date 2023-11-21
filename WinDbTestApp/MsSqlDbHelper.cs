89172b3b2e2a80b6541d2615aac07413b846f5f2
OksanaNSukhobok <93849417+OksanaNSukhobok@users.noreply.github.com>
2023-11-21 12:19:15 +0500

    Установка связи с MS SQL сервером

--- WinDbTestApp/MsSqlDbHelper.cs
+++ WinDbTestApp/MsSqlDbHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,25 @@ namespace WinDbTestApp
     {
         public static string DoDbTest()
         {
-            string s = "Hello!";
+            string s = " ";
 
-            return s;
+            string strCnn = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true";
+            using (var cnn = new SqlConnection(strCnn))
+            {
+                var cmd = cnn.CreateCommand();
+                cmd.CommandText = "SELECT 1";
+                cnn.Open();
+
+                var rdr = cmd.ExecuteReader();
+
+                while (rdr.Read()) 
+                {
+                    // get the results of each column
+                    s += (string)rdr[0].ToString() + " ";
+                }
+            }
+
+                return s;
         }
     }
 }
